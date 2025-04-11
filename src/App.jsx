227c8ab2edbbfcd7e113354a25d9f664a8b04e46
tebohostols <tebohogarfield@gmail.com>
2025-04-11 13:37:50 +0200

    first commit. Started with an apple website few days ago

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Highlights from './components/Highlights';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Model from './components/Model';
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero/>
+      <Highlights/>
+      <Model/>
+    </main>
+  )
+}
+
+export default App
